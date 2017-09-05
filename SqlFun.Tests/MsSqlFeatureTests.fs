4e96fbd586cbe1603ecef3c9303631d3b00f3982
jacentino <jacentino@gmial.com>
2017-09-05 21:58:15 +0200

    Optimizing TVP creation

--- SqlFun.Tests/MsSqlFeatureTests.fs
+++ SqlFun.Tests/MsSqlFeatureTests.fs
@@ -12,7 +12,32 @@ module MsSqlTestQueries =
     
     let sql command = MsSql.sql createConnection defaultParamBuilder command
 
+    type Tag2 = {
+        postId: int option
+        name: string
+    }
+
+    type Tag3 = {
+        tag: Tag
+    }
+
+    type Tag4 = {
+        tagOpt: Tag option
+    }
+
     let updateTags: Post -> DataContext -> unit = 
+        sql "delete from tag where postId = @id;
+             insert into tag (postId, name) select @id, name from @tags"
+
+    let updateTags2: int -> Tag2 list -> DataContext -> unit = 
+        sql "delete from tag where postId = @id;
+             insert into tag (postId, name) select @id, name from @tags"
+
+    let updateTags3: int -> Tag3 list -> DataContext -> unit = 
+        sql "delete from tag where postId = @id;
+             insert into tag (postId, name) select @id, name from @tags"
+
+    let updateTags4: int -> Tag4 list -> DataContext -> unit = 
         sql "delete from tag where postId = @id;
              insert into tag (postId, name) select @id, name from @tags"
 
@@ -29,14 +54,50 @@ type MsSqlTests() =
     member this.``Queries utilizing TVP-s are executed correctly.``() = 
         let p = Tooling.getPost 2 |> run |> Option.get
         let tags = [
-            { postId = p.id; name = "EntityFramework" } 
-            { postId = p.id; name = "Dapper" }
-            { postId = p.id; name = "FSharp.Data.SqlClient" }
+            { Tag.postId = p.id; name = "EntityFramework" } 
+            { Tag.postId = p.id; name = "Dapper" }
+            { Tag.postId = p.id; name = "FSharp.Data.SqlClient" }
         ]
         updateTags { p with tags = tags } |> run
-        let tags = Tooling.getTags p.id |> run
-        Assert.AreEqual(3, tags |> List.length)
+        let result = Tooling.getTags p.id |> run
+        Assert.AreEqual(tags |> List.sortBy (fun t -> t.name), result |> List.sortBy (fun t -> t.name))
         
+    [<Test>]
+    member this.``Queries utilizing TVP-s with optional fields are executed correctly.``() = 
+        let tags = [
+            { Tag2.postId = Some 2; name = "EntityFramework" } 
+            { Tag2.postId = Some 2; name = "Dapper" }
+            { Tag2.postId = None; name = "FSharp.Data.SqlClient" }
+        ]
+        updateTags2 2 tags |> run
+        let result = Tooling.getTags 2 |> run |> List.map (fun t -> t.name)
+        Assert.AreEqual(tags |> List.map (fun t -> t.name) |> List.sortBy id, result |> List.sortBy id)
+        
+    [<Test>]
+    member this.``Queries utilizing TVP-s with non-optional substructures are executed correctly.``() = 
+        let tags = [
+            { tag = { Tag.postId = 2; name = "EntityFramework" } }
+            { tag = { Tag.postId = 2; name = "Dapper" } }
+            { tag = { Tag.postId = 2; name = "FSharp.Data.SqlClient" } }
+        ]
+        updateTags3 2 tags |> run
+        let result = Tooling.getTags 2 |> run |> List.map (fun t -> { tag = t })
+        Assert.AreEqual(tags |> List.sortBy (fun t -> t.tag.name), result |> List.sortBy (fun t -> t.tag.name))
+        
+    [<Test>]
+    member this.``Queries utilizing TVP-s with optional substructures are executed correctly.``() = 
+        let tags = [
+            { tagOpt = Some { Tag.postId = 2; name = "EntityFramework" } }
+            { tagOpt = Some { Tag.postId = 2; name = "Dapper" } }
+            { tagOpt = Some { Tag.postId = 2; name = "FSharp.Data.SqlClient" } }
+        ]
+        updateTags4 2 tags |> run
+        let result = Tooling.getTags 2 |> run |> List.map (fun t -> { tagOpt = Some t })
+        Assert.AreEqual(tags |> List.sortBy (fun t -> t.tagOpt.Value.name), result |> List.sortBy (fun t -> t.tagOpt.Value.name))
+        
+    
+
+
     
 
 
--- SqlFun/MsSql.fs
+++ SqlFun/MsSql.fs
@@ -10,33 +10,10 @@ module MsSql =
     open Microsoft.FSharp.Reflection
     open System.Reflection
     open Future
+    open Microsoft.SqlServer.Server
+    open System.Collections.Concurrent
 
     let defaultParamBuilder = defaultParamBuilder
-
-    let private getColumnType propType = 
-        if isOption propType
-        then getUnderlyingType propType
-        elif propType.IsEnum
-        then 
-            if propType.GetFields() |> Seq.exists (fun f -> f.IsStatic && not (f.GetCustomAttributes<EnumValueAttribute>() |> Seq.isEmpty))
-            then typeof<string>
-            else typeof<int>
-        else propType
-
-    let rec private getAllRecFields (recType: Type) = 
-        if FSharpType.IsTuple recType
-        then 
-            FSharpType.GetTupleElements recType
-            |> Seq.collect getAllRecFields
-        else
-            FSharpType.GetRecordFields recType
-            |> Seq.collect (fun p -> if isCollectionType p.PropertyType
-                                        then Seq.empty
-                                        elif isSimpleType p.PropertyType || isSimpleTypeOption p.PropertyType
-                                        then Seq.singleton (p.Name, getColumnType p.PropertyType)
-                                        elif isOption p.PropertyType
-                                        then getAllRecFields (getUnderlyingType p.PropertyType)
-                                        else getAllRecFields p.PropertyType)
 
     let private getEnumValue (value: obj) = 
         value.GetType().GetFields()
@@ -54,37 +31,89 @@ module MsSql =
         then getEnumValue value
         else value
 
-    let rec private getAllRecValues (recType: Type) (item: obj) = 
+    let private composeAll (f: obj -> obj) (l: (string * (obj -> obj)) seq) = 
+        l |> Seq.map (fun (n, f1) -> n, fun item -> let x = f item in if x = null then null else f1 x)
+
+    let rec private getAccessors (recType: Type): (string * (obj -> obj)) seq = 
         if FSharpType.IsTuple recType
         then 
             FSharpType.GetTupleElements recType
-            |> Seq.mapi (fun i t -> getAllRecValues t (recType.GetProperty("Item" + (i + 1).ToString()).GetValue item))
+            |> Seq.mapi (fun i t -> composeAll (fun item -> recType.GetProperty("Item" + (i + 1).ToString()).GetValue item) (getAccessors t))
             |> Seq.collect id
         else
             FSharpType.GetRecordFields recType
             |> Seq.collect (fun p -> if isCollectionType p.PropertyType
                                         then Seq.empty
                                         elif isSimpleType p.PropertyType || isSimpleTypeOption p.PropertyType
-                                        then Seq.singleton (convertToColumnValue (p.GetValue item))
+                                        then Seq.singleton (p.Name, fun item -> convertToColumnValue (p.GetValue item))
                                         elif isOption p.PropertyType
                                         then 
-                                            if item <> null 
-                                            then getAllRecValues p.PropertyType (p.GetValue (item.GetType().GetProperty("Value").GetValue item))
-                                            else Seq.singleton item                                        
-                                        else getAllRecValues p.PropertyType (p.GetValue item))
+                                            let underlyingType = p.PropertyType.GetGenericArguments().[0]
+                                            composeAll 
+                                                (fun item -> 
+                                                    let opt = p.GetValue item
+                                                    let value = p.PropertyType.GetProperty("Value").GetValue opt
+                                                    value)
+                                                (getAccessors underlyingType)
+                                        else 
+                                            composeAll  (fun item -> p.GetValue item) (getAccessors p.PropertyType))
 
-    let private toDataTable (items: obj, itemType: Type): DataTable = 
-        let itemSeq = items :?> obj seq
-        let table = new DataTable()
-        table.TableName <- itemType.Name
-        getAllRecFields(itemType) |> Seq.iter (table.Columns.Add >> ignore)
-        itemSeq |> Seq.iter (getAllRecValues itemType >> Array.ofSeq >> table.Rows.Add >> ignore)
-        table
 
-    let private MsSqlParamBuilder defaultPB prefix name (expr: Expression) names = 
+    let private createMetaData name typeName (maxLen: int64) (precision: byte) (scale: byte) = 
+        let dbType = Enum.Parse(typeof<SqlDbType>, typeName, true) :?> SqlDbType
+        match dbType with
+        | SqlDbType.Char 
+        | SqlDbType.NChar 
+        | SqlDbType.VarChar 
+        | SqlDbType.NVarChar -> SqlMetaData(name, dbType, Math.Min(maxLen, 4000L))
+        | SqlDbType.Binary 
+        | SqlDbType.VarBinary -> SqlMetaData(name, dbType, maxLen)
+        | SqlDbType.Decimal 
+        | SqlDbType.Money 
+        | SqlDbType.SmallMoney -> SqlMetaData(name, dbType, precision, scale)
+        | _ -> SqlMetaData(name, dbType)
+
+    let private getMetaData (t: Type) (connection: IDbConnection) =
+        use command = connection.CreateCommand()
+        command.CommandText <- "select c.name, t.name as typeName, c.max_length, c.precision, c.scale, c.is_nullable
+                                from sys.table_types tt 
+	                                join sys.columns c on c.object_id = tt.type_table_object_id
+	                                join sys.types t on t.system_type_id = c.system_type_id and t.user_type_id = c.user_type_id
+                                where tt.name = @name"
+        let param = command.CreateParameter()
+        param.ParameterName <- "@name"
+        param.Value <- t.Name
+        command.Parameters.Add(param) |> ignore
+        use reader = command.ExecuteReader()
+        [| 
+            while reader.Read() do
+                yield createMetaData (reader.GetString 0) (reader.GetString 1) (int64(reader.GetInt16 2)) (reader.GetByte 3) (reader.GetByte  4)
+        |]
+
+    let private getRecSequenceBuilder (connection: IDbConnection) (itemType: Type) = 
+        let metadata = getMetaData itemType connection
+        let record = SqlDataRecord(metadata)
+        let accessors = getAccessors itemType |> Map.ofSeq
+        fun (items: obj) ->
+            let itemSeq = items :?> obj seq
+            if itemSeq |> Seq.length = 0 then null
+            else
+                seq {
+                    for item in itemSeq do
+                        for i in 0..metadata.Length - 1 do
+                            let v = accessors.[metadata.[i].Name] item
+                            record.SetValue(i, v) 
+                        yield record
+                }
+
+    let private MsSqlParamBuilder (connectionBuilder: unit -> #IDbConnection) defaultPB prefix name (expr: Expression) names = 
         if isCollectionType expr.Type && isComplexType (getUnderlyingType expr.Type)
         then
             let itemType = getUnderlyingType expr.Type
+            let typeName = itemType.Name
+            use connection = connectionBuilder()
+            connection.Open()
+            let toSqlDataRecords = getRecSequenceBuilder connection itemType
             [
                 prefix + name,
                 expr,
@@ -92,21 +121,21 @@ module MsSql =
                     let param = new SqlClient.SqlParameter()
                     param.ParameterName <- "@" + name
                     if value <> null then
-                        param.Value <- toDataTable (value, itemType)
+                        param.Value <- toSqlDataRecords value
                     param.SqlDbType <- SqlDbType.Structured
-                    param.TypeName <- itemType.Name
+                    param.TypeName <- typeName
                     command.Parameters.Add(param)
                 ,
-                [] :> obj
+                null
             ]       
         else
             defaultPB prefix name expr names
 
     let sql connectionBuilder paramBuilder commandText = 
-        sql connectionBuilder (fun defaultPB -> paramBuilder <| MsSqlParamBuilder defaultPB) commandText
+        sql connectionBuilder (fun defaultPB -> paramBuilder <| MsSqlParamBuilder connectionBuilder defaultPB) commandText
 
     let storedproc connectionBuilder paramBuilder procName = 
-        storedproc connectionBuilder (fun defaultPB -> paramBuilder <| MsSqlParamBuilder defaultPB) procName
+        storedproc connectionBuilder (fun defaultPB -> paramBuilder <| MsSqlParamBuilder connectionBuilder defaultPB) procName
 
 
 
