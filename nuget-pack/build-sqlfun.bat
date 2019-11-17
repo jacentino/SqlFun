a1ea901f802b0d7a9208cf37eedf5cfd50cd8cd9
Jacek <jacentino@gmail.com>
2019-11-17 22:13:28 +0100

    Moved to fake script.

--- nuget-pack/build-sqlfun.bat
+++
@@ -1,5 +0,0 @@
-msbuild ..\SqlFun.sln /p:Configuration=release
-rd /S /Q .\lib
-echo f | xcopy ..\SqlFun\bin\release\SqlFun.dll .\lib\net461\SqlFun.dll
-echo f | xcopy ..\SqlFun\bin\release\SqlFun.XML .\lib\net461\SqlFun.XML
-nuget pack .\SqlFun.nuspec
